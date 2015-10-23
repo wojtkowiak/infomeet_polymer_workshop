6307096f21c83a4608572a810fe15511c2743aae
Bartosz Wojtkowiak <b.wojtkowiak@cctechnology.pl>
2015-10-23 12:17:07 +0200

    transpile

--- gulpfile.js
+++ gulpfile.js
@@ -18,9 +18,54 @@ $.helpers = {
 
 var watching = false;
 
-// Fixes the paths and copies html files to dist.
-gulp.task('html', function () {
+gulp.task('extractJS', function () {
     return gulp.src('./my-*/*.html')
+        .pipe($.crisper())
+        .pipe(gulp.dest('./dist/'));
+});
+
+// Wraps the stream, so we can end it while in 'watch' task
+function wrap(stream) {
+    stream.on('error', function (error) {
+        $.util.log($.util.colors.red(error.message));
+        $.util.log(error.stack);
+        if (watching) {
+            $.util.log($.util.colors.yellow('[aborting]'));
+            stream.end();
+        } else {
+            $.util.log($.util.colors.yellow('[exiting]'));
+            console.log('rrr');
+            process.exit(1);
+        }
+    });
+    return stream;
+}
+
+gulp.task('transpileJS', ['extractJS'], function () {
+    var dirs = fs.readdirSync('./dist/'), streams = $.merge(), es2015File;
+    for (var i = 0; i < dirs.length; i++) {
+        var files = fs.readdirSync('./dist/' + dirs[i]);
+        for (var k = 0; k < files.length; k++) {
+            if (files[k].indexOf('.js') != -1 && files[k].indexOf('.js') === files[k].length - 3) {
+                es2015File = fs.readFileSync('./' + dirs[i] + '/' + files[k].replace('.js', '.html'), 'utf8').indexOf('text/ecmascript-6') > -1;
+                if (es2015File) {
+                    streams.add(
+                        gulp.src('./dist/' + dirs[i] + '/' + files[k])
+                            .pipe($.sourcemaps.init())
+                            .pipe(wrap($.babel({})))
+                            .pipe($.sourcemaps.write(''))
+                            .pipe(gulp.dest('./dist/' + dirs[i] + '/'))
+                    );
+                }
+            }
+        }
+    }
+    return streams;
+});
+
+// Fixes the paths and copies html files to dist.
+gulp.task('html', ['transpileJS'], function () {
+    return gulp.src('./dist/my-*/*.html')
         .pipe($.replace(/\.\.\/(?!my)/gmi, '../../'))
         .pipe(gulp.dest('./dist/'));
 });
